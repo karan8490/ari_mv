63387e054d899963f4ef9188756197aaea4df571
Saurabh Chouhan <schouhan@aripratech.com>
2016-07-11 01:22:12 +0530

    Added following APIs GET /user?self GET /project/<projectid> GET /topology GET /solution GET /environment/<environmentid> GET /solutiondeployement GET /solutiondeployement/<id>

--- src/main/java/com/kpmg/mw/rest/LoginController.java
+++ src/main/java/com/kpmg/mw/rest/LoginController.java
@@ -1,12 +1,15 @@
 package com.kpmg.mw.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.Optional;
-import java.util.Set;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +27,6 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.kpmg.mw.config.CommonConfigBean;
-import com.kpmg.mw.vo.ResponseVO;
 
 @RestController
 public class LoginController {
@@ -37,24 +39,25 @@ public class LoginController {
 
 	private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ResponseVO authenticate(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping(value = "/login", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String authenticate(HttpServletRequest request, HttpServletResponse response) {
 
-		logger.info("Inside authenticate method");
-
-		final String uri = commonConfigBean.getBaseURL() + "/j_spring_security_check";
+		final String uriString = commonConfigBean.getCompleteRequestURI("user?self");
 		HttpHeaders headers = new HttpHeaders();
-		MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+		String user = request.getParameter("user");
+		String password = request.getParameter("password");
 
-		body.add("j_username", request.getParameter("username"));
-		body.add("j_password", request.getParameter("password"));
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		HttpEntity<Object> entity = new HttpEntity<Object>(body, headers);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
 		ResponseEntity<String> responseEntity = null;
 		try {
-			responseEntity = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
-			logger.info("headers" + responseEntity.getHeaders());
+
+			URI uri = new URIBuilder(uriString).addParameter("user", user).addParameter("password", password).build();
+			responseEntity = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
 
 			String JID = null;
 			Optional<String> jsessionID = responseEntity.getHeaders().get("Set-Cookie").stream()
@@ -63,18 +66,17 @@ public class LoginController {
 				JID = jsessionID.get().substring(jsessionID.get().indexOf("="));
 			}
 
-			logger.info("JID value is " + JID);
 			response.addCookie(new Cookie("JSESSIONID", JID));
-			return new ResponseVO(true, "Login successful");
+			return responseEntity.getBody();
+
 		} catch (RestClientException e) {
-			Cookie cookie = new Cookie("JSESSIONID", null);
-			cookie.setMaxAge(0);
-			response.addCookie(cookie);
+			logger.error("error occurred", e);
 			if (responseEntity != null)
 				response.setStatus(responseEntity.getStatusCodeValue());
-			return new ResponseVO(false, "Login failed");
+			return null;
+		} catch (URISyntaxException e) {
+			logger.error("URI syntax error");
+			return null;
 		}
-
 	}
-
 }
--- src/main/java/com/kpmg/mw/rest/SolutionRestController.java
+++ src/main/java/com/kpmg/mw/rest/SolutionRestController.java
@@ -2,8 +2,6 @@ package com.kpmg.mw.rest;
 
 import java.util.Arrays;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +18,9 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.kpmg.mw.config.CommonConfigBean;
-import com.kpmg.mw.vo.ProjectVO;
-
-/**
- * This controller would contain all the REST APIs for Project resource
- * 
- * @author
- *
- */
 
 @RestController
-public class ProjectRestController {
+public class SolutionRestController {
 
 	private Logger logger = LoggerFactory.getLogger(ProjectRestController.class);
 
@@ -40,63 +30,9 @@ public class ProjectRestController {
 	@Autowired
 	private CommonConfigBean commonConfigBean;
 
-	/**
-	 * Returns details of project with passed identifier
-	 * 
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/project", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public String projects() {
-
-		final String uriString = commonConfigBean.getCompleteRequestURI("project");
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		headers = new HttpHeaders();
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		try {
-			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.GET, entity,
-					String.class);
-			String response = responseEntity.getBody();
-			logger.debug("Received response as " + response);
-			return response;
-		} catch (RestClientException e) {
-			logger.error(e.getMessage(), e);
-			return null;
-		}
-	}
-
-	/**
-	 * Returns details of project with passed identifier
-	 * 
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/project/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public String projectDetails(@PathVariable("id") String id) {
-
-		final String uriString = commonConfigBean.getCompleteRequestURI("project") + "/" + id;
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		headers = new HttpHeaders();
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		try {
-			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.GET, entity,
-					String.class);
-			String response = responseEntity.getBody();
-			logger.debug("Received response as " + response);
-			return response;
-		} catch (RestClientException e) {
-			logger.error(e.getMessage(), e);
-			return null;
-		}
-	}
-
-	@RequestMapping(value = "/topology", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/solution", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String deploymentNames() {
-		final String uriString = commonConfigBean.getCompleteRequestURI("topology");
+		final String uriString = commonConfigBean.getCompleteRequestURI("solution");
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		headers = new HttpHeaders();
@@ -114,9 +50,29 @@ public class ProjectRestController {
 		}
 	}
 
-	@RequestMapping(value = "/environment/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public String workloadStatus(@PathVariable("id") String id) {
-		final String uriString = commonConfigBean.getCompleteRequestURI("environment") + "/" + id;
+	@RequestMapping(value = "/solutiondeployment", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String solutionDeployment() {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.GET, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+
+	@RequestMapping(value = "/solutiondeployment/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String solutionDeploymentDetails(@PathVariable("id") String id) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id;
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		headers = new HttpHeaders();
