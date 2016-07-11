4386e822d82d9e905c52b9126e19f823a02645b4
Saurabh Chouhan <schouhan@aripratech.com>
2016-07-11 21:50:51 +0530

    Added following APIs
    PUT "solutiondeployment/{id}/deploy
    PUT "solutiondeployment/{id}/start
    PUT "solutiondeployment/{id}/stop
    PUT "solutiondeployment/{id}/relese
    PUT "solutiondeployment/{id}/promote/{locationid}
    POST "solutiondeployment/{id}/approval
    DELETE "solutiondeployment/{id}

--- src/main/java/com/kpmg/mw/rest/SolutionRestController.java
+++ src/main/java/com/kpmg/mw/rest/SolutionRestController.java
@@ -30,6 +30,10 @@ public class SolutionRestController {
 	@Autowired
 	private CommonConfigBean commonConfigBean;
 
+	/**
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/solution", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String deploymentNames() {
 		final String uriString = commonConfigBean.getCompleteRequestURI("solution");
@@ -49,6 +53,11 @@ public class SolutionRestController {
 			return null;
 		}
 	}
+	
+	/**
+	 * Get all solution deployment 
+	 * @return
+	 */
 
 	@RequestMapping(value = "/solutiondeployment", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String solutionDeployment() {
@@ -70,6 +79,11 @@ public class SolutionRestController {
 		}
 	}
 
+	/**
+	 * Get solution details by identifier
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(value = "/solutiondeployment/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String solutionDeploymentDetails(@PathVariable("id") String id) {
 		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id;
@@ -80,6 +94,186 @@ public class SolutionRestController {
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 		try {
 			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.GET, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+
+	/**
+	 * Deploy a solution
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/solutiondeployment/{id}/deploy", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String deploySolution(@PathVariable("id") String id) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id + "/deploy";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.PUT, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+	
+	/**
+	 * Starts a solution
+	 * @param id
+	 * @return
+	 */
+
+	@RequestMapping(value = "/solutiondeployment/{id}/start", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String startSolution(@PathVariable("id") String id) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id + "/start";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.PUT, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+	
+	/**
+	 * Stops a solution
+	 * @param id
+	 * @return
+	 */
+
+	@RequestMapping(value = "/solutiondeployment/{id}/stop", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String stopSolution(@PathVariable("id") String id) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id + "/stop";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.PUT, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+	
+	/**
+	 * Releases a solution
+	 * @param id
+	 * @return
+	 */
+
+	@RequestMapping(value = "/solutiondeployment/{id}/release", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String releaseSolution(@PathVariable("id") String id) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id + "/release";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.PUT, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+
+	/**
+	 * Approves a soution
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/solutiondeployment/{id}/approval", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String approveSolution(@PathVariable("id") String id) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id + "/approval";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.POST, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+	/**
+	 * Promotes a solution to other location
+	 * @param id
+	 * @param locationId
+	 * @return
+	 */
+
+	@RequestMapping(value = "/solutiondeployment/{id}/promote/{locationId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String promoteSolution(@PathVariable("id") String id, @PathVariable("locationId") String locationId) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id + "/promote/"
+				+ locationId;
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.PUT, entity,
+					String.class);
+			String response = responseEntity.getBody();
+			logger.debug("Received response as " + response);
+			return response;
+		} catch (RestClientException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+
+	/**
+	 * Removes a solution
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/solutiondeployment/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String removeSolution(@PathVariable("id") String id) {
+		final String uriString = commonConfigBean.getCompleteRequestURI("solutiondeployment") + "/" + id;
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uriString, HttpMethod.DELETE, entity,
 					String.class);
 			String response = responseEntity.getBody();
 			logger.debug("Received response as " + response);
